067c2a2e856e4f4e58da8e76fff830766d455a1f
LabComMT-3 <Got153937>
2020-12-29 11:41:37 +0700

    Lab2

---
+++ src/LAB2/BasicInputData.java
@@ -0,0 +1,13 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class BasicInputData {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+        System.out.print("The number is "+num);
+    }
+}
